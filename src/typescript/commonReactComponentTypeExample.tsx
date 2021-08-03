0b1767a0b2928bb9d7c760c154f4ae4dcb5b61a2
Ruslan Sultygov <Ruslan.Sultygov@ihsmarkit.com>
2021-08-03 12:13:21 +0300

    Common components type exp

---
+++ src/typescript/commonReactComponentTypeExample.tsx
@@ -0,0 +1,33 @@
+
+
+export interface MyPropsType {
+    id: 'A' | 'B' | "C";
+    label: string;
+}
+export interface ConfigElement extends MyPropsType {
+component:
+    React.ComponentType<MyPropsType>;
+}
+
+export interface SomeMainComponentProps {
+    elements: ConfigElement[];
+}
+
+// отрендерит массив классовых или функциональных компонентов
+// render array of functional or class components
+export function SomeMainComponent({ elements }: SomeMainComponentProps) {
+
+return (
+    <>
+    {elements.map(({ id, label, component: ComponentElement}) => {
+        return (
+        <ComponentElement
+            id={id}
+            key={Array.isArray(id) ? (id[0] as string) : (id as string)}
+            label={label}
+        />
+        );
+    })}
+    </>
+);
+}
