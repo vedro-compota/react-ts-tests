0f5683135c65d4ed85c45479b2bf8cdf2e838650
vedro-compota <ru-ibn-hottab@yandex.ru>
2021-08-16 17:27:25 +0300

    up to 12 selectors only - demo

---
+++ src/typescript/ErorrsDemo/ReselectDelectorsCombination/ReselectDelectorsCombination.ts
@@ -0,0 +1,42 @@
+import { createSelector } from '@reduxjs/toolkit';
+
+type UserInfo = {
+    name: string,
+    active: boolean
+    items: string[]
+}
+
+type RootState = {
+    user: UserInfo
+}
+
+export const initialState: UserInfo = {
+     name: '',
+     items: [],
+     active: true
+  };
+
+const selectDomain = (state: RootState) => state?.user || initialState;
+const selectUserName = createSelector([selectDomain], ({ name }) => name);
+const selectIsActive = createSelector([selectDomain], ({ active }) => active);
+
+const selectDemo1 = createSelector([selectDomain], ({ name }) => name);
+
+
+const selectUserInfo = createSelector([selectUserName, 
+    selectIsActive, 
+    // uncomment selector below to see error: 
+    // selectDemo1, 
+    // selectDemo1,
+    // selectDemo1,
+    // selectDemo1,
+    // selectDemo1,
+    // selectDemo1,
+    // selectDemo1,
+    // selectDemo1,
+    // selectDemo1,
+    // selectDemo1,
+    // selectDemo1, 
+],
+     (name, active ) => `${name} ${active}`);
+
--- src/typescript/commonReactComponentTypeExample.tsx
+++ src/typescript/commonReactComponentTypeExample.tsx
@@ -1,4 +1,4 @@
-
+import React from 'react';
 
 export interface MyPropsType {
     id: 'A' | 'B' | "C";
